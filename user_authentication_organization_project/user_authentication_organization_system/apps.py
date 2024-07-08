60947cc1101c9794cc36f8ece4b3040962febbd3
EmmanuelJunior <142091756+EmmanuelJunuior@users.noreply.github.com>
2024-07-09 01:22:32 +0200

    Delete user_authentication_organization_project directory

--- user_authentication_organization_project/user_authentication_organization_system/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserAuthenticationOrganizationSystemConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user_authentication_organization_system'
